82abf0afdc51a7e84a291de4fdc8c8bc76d34e75
Imamunnisa <imamunnisa1999@gmail.com>
2020-12-27 20:22:32 +0530

    This is a java project

---
+++ demoproject/src/main/java/com/example/project/demoproject/model/project.java
@@ -0,0 +1,11 @@
+package com.example.project.demoproject.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "project")
+public class project {
+    
+    
+}
